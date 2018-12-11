ae4ef8c3be996f2e02848e73e51bb3fe04c012ea
ian-fisher <yanfisher13@gmail.com>
2018-12-11 19:35:22 +0000

    Add files via upload

---
+++ Bubble disco.pas
@@ -0,0 +1,26 @@
+﻿Uses GraphABC;
+
+var
+  r, g, b, x1, y1, x, y, r1, k, n: integer;
+
+procedure Bubble;
+begin
+  r := random(0, 255);
+  g := random(0, 255);
+  b := random(0, 255);
+  r1 := random(8, 40);
+  x := random(r1, x1 - r1);
+  y := random(r1, y1 - r1);
+  SetBrushColor(RGB(r, g, b));
+  Circle(x, y, r1);
+end;
+
+begin
+  SetWindowTitle('Bubble Disco');
+  read(x1, y1, n);
+  SetWindowSize(x1, y1);
+  for k := 1 to n do
+  begin
+    Bubble;
+  end;
+end.
