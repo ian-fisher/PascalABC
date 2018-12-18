2db97dd38a974acb500258cc6e5a6d61684240fc
ian-fisher <yanfisher13@gmail.com>
2018-12-18 20:24:14 +0000

    Add files via upload

---
+++ Drawing.pas
@@ -0,0 +1,23 @@
+﻿uses GraphABC;
+
+procedure MouseDown(x,y,mb: integer);
+begin
+  MoveTo(x,y);
+end;
+
+procedure MouseMove(x,y,mb : integer);
+begin
+  if mb=1 then LineTo(x,y);
+end;
+
+procedure MouseUp(x, y, mb : integer);
+begin
+  SetPenColor(clRandom);
+end;
+
+begin
+  OnMouseMove := MouseMove;
+  OnMouseDown := MouseDown;
+  OnMouseUp := MouseUp;
+end.
+
